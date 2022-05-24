731cc93e0485ddd7381277b59c9f688c9ebcac8b
Musfakur <procreationsbliss@gmail.com>
2022-05-24 21:36:33 +0600

    use token done

--- src/Pages/Authentication/Signup.js
+++ src/Pages/Authentication/Signup.js
@@ -4,6 +4,7 @@ import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 import { Link, useNavigate } from 'react-router-dom';
+import useToken from '../../hooks/useToken';
 
 const Signup = () => {
 
@@ -18,6 +19,8 @@ const Signup = () => {
 
   const [updateProfile, updating, updateError] = useUpdateProfile(auth);
 
+  const [token] = useToken(user || gUser);
+
   const navigate = useNavigate();
 
   let signInError;
@@ -30,15 +33,14 @@ const Signup = () => {
     signInError = <p className='text-error mb-5'>{error?.message || gError?.message || updateError?.message}</p>
   }
 
-  if (user || gUser) {
-    console.log(user || gUser);
+  if (token) {
+    navigate('/');
   }
 
   const onSubmit = async data => {
     await createUserWithEmailAndPassword(data.email, data.password);
     await updateProfile({ displayName: data.name });
     console.log('Update done');
-    navigate('/');
   };
 
 
