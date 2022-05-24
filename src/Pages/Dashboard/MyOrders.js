6f096ab5a91cdd7d97e9b4e593a34c75e9f5c018
Musfakur <procreationsbliss@gmail.com>
2022-05-24 19:51:27 +0600

    order created

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyOrders = () => {
+
+  const [orders, setOrders] = useState([]);
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/order?userEmail=${user.email}`)
+        .then(res => res.json())
+        .then(data => setOrders(data));
+    }
+  }, [user])
+
   return (
     <div>
-      <h1>My Orders Page</h1>
+      <h1>My Orders {orders.length}</h1>
     </div>
   );
 };
