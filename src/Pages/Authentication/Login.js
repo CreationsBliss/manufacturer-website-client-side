acbf323d99b0e16ed736169286dec9c12cb6ec79
Musfakur <procreationsbliss@gmail.com>
2022-05-24 21:53:45 +0600

    use toke generated

--- src/Pages/Authentication/Login.js
+++ src/Pages/Authentication/Login.js
@@ -4,6 +4,7 @@ import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import useToken from '../../hooks/useToken';
 
 const Login = () => {
 
@@ -15,6 +16,8 @@ const Login = () => {
     loading,
     error,
   ] = useSignInWithEmailAndPassword(auth);
+
+  const [token] = useToken(user || gUser);
 
   let signInError;
   let navigate = useNavigate();
@@ -29,7 +32,7 @@ const Login = () => {
     signInError = <p className='text-error mb-5'>{error?.message || gError?.message}</p>
   }
 
-  if (user || gUser) {
+  if (token) {
     navigate(from, { replace: true });
   }
 
